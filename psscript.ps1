fc978907eaf2f82fc42000a0fd96b0515f1d1e57
chakri42 <42836144+chakri42@users.noreply.github.com>
2022-02-14 20:17:37 +0530

    Update psscript.ps1

--- psscript.ps1
+++ psscript.ps1
@@ -1,1 +1,68 @@
+Param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AzureUserName,
 
+    [string]
+    $AzurePassword,
+
+    [string]
+    $AzureTenantID,
+
+    [string]
+    $AzureSubscriptionID,
+
+    [string]
+    $ODLID,
+
+    [string]
+    $InstallCloudLabsShadow,
+
+    [string]
+    $DeploymentID
+)
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+
+#Import Common Functions
+$path = pwd
+$path=$path.Path
+$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
+#Create C:\CloudLabs
+New-Item -ItemType directory -Path C:\CloudLabs -Force
+
+# Run Imported functions from cloudlabs-windows-functions.ps1
+WindowsServerCommon
+InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID
+
+sleep 5
+
+InstallAzPowerShellModule
+InstallAzCLI
+InstallEdgeChromium
+
+
+Enable-CloudLabsEmbeddedShadow azureuser trainer Password.1!!
+
+sleep 10
+
+#Download git repository
+New-Item -ItemType directory -Path C:\AllFiles
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://github.com/CloudLabs-MOC/AZ500-AzureSecurityTechnologies/archive/prod.zip","C:\AllFiles\AllFiles.zip")
+#unziping folder
+function Expand-ZIPFile($file, $destination)
+{
+$shell = new-object -com shell.application
+$zip = $shell.NameSpace($file)
+foreach($item in $zip.items())
+{
+$shell.Namespace($destination).copyhere($item)
+}
+}
+Expand-ZIPFile -File "C:\AllFiles\AllFiles.zip" -Destination "C:\AllFiles\"
